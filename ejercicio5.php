378f4a885ce3e21142e62b8e4629ef05d8dfa9dc
ArturRiosDev <arturo.rioscueto@gmail.com>
2025-06-28 07:42:48 -0600

    tipos de datos

---
+++ ejercicio5.php
@@ -0,0 +1,10 @@
+<?php
+
+    $Edad=32; //Entero  
+    $Nombre="Arturo "; //string
+    $Altura=1.4; //numero flotante
+
+    $Programador=null;
+
+    echo "El nombre es ".$Nombre. "la edad es ".$Edad."su puesto es ".$Programador
+?>
