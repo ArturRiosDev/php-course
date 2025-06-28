2fa456b31045e59f21980353a3c5f85dbe587987
ArturRiosDev <arturo.rioscueto@gmail.com>
2025-06-28 07:48:19 -0600

    constantes

---
+++ ejercicio6.php
@@ -0,0 +1,18 @@
+<?php
+
+$edad =32; //variables
+
+echo $edad."<br/>";
+
+$edad = 40;// cambiar de valor de la variable
+
+echo $edad."<br/>";
+
+//uso de constante 
+define("NOMBRE","arturo");
+
+
+
+//imprimir constante
+echo NOMBRE;
+?>
