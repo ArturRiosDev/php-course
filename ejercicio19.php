5b80df492f646f690992fb1c8fd96e0532b0e27d
ArturRiosDev <arturo.rioscueto@gmail.com>
2025-06-29 06:55:44 -0600

    arreglos

---
+++ ejercicio19.php
@@ -0,0 +1,7 @@
+<?php
+
+//de esta manera en lugar de que su indice sea 0,1,2 , ahora seran los que nosotros directamente estamos poniendo
+    $frutas=array("f"=>"fresa","m"=>"manzana","p"=>"pera");
+    print_r($frutas);
+
+?>
