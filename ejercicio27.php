5119f6142f62cd97d535c85a72a3a937c23ba4e0
ArturRiosDev <arturo.rioscueto@gmail.com>
2025-06-29 12:19:58 -0600

    como poner una consulta

--- ejercicio27.php
+++ ejercicio27.php
@@ -8,6 +8,10 @@
             $conexion = new PDO("mysql:host=$servidor;dbname=album;charset=utf8", $usuario, $password);
             // Opcional: establecer el modo de errores de PDO
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql ="INSERT INTO `fotos` (`id`,`nombre`,`ruta`) VALUES (NULL,'jugando con la programacion','foto.jpg')";
+
+            $conexion->exec($sql);
             echo "Conexion exitosa";
         } catch (PDOException $error) {
             echo "Conexion erronea ".$error;
