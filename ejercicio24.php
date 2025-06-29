d7c2006081acd9a92cb8a5976f730fc5aeddebaf
ArturRiosDev <arturo.rioscueto@gmail.com>
2025-06-29 07:38:07 -0600

    herencia

---
+++ ejercicio24.php
@@ -0,0 +1,51 @@
+<?php
+
+class Persona
+{
+    private $edad; //solo tienes acceso desde la propia clase 
+    public $nombre; //propiedades
+    protected $altura; //solo se accede desde las clases o clases heredadas
+    public function asignarNombre($nuevoNombre)
+    { //acciones o metodos....
+        $this->nombre = $nuevoNombre;
+    }
+    public function imprimirNombre(){
+        echo "Hola soy ".$this->nombre;
+    }
+    public function mostrarNombre(){
+        $this->edad =20;
+        return $this->edad;
+    }
+}
+
+class Trabajador extends Persona{
+    public $puesto;
+
+    public function presentarseComoTrabajador(){
+        echo "hola soy ".$this->puesto;
+    }
+
+}
+
+$primerTrabajador = new Trabajador();
+$primerTrabajador->asignarNombre("Arturito");
+$primerTrabajador->puesto="Profesor";
+
+$primerTrabajador->presentarseComoTrabajador();
+
+
+
+$objAlumno = new Persona(); //creacion de un objeto
+
+$objAlumno->asignarNombre("Arturo"); //llamando un metodo 
+
+
+echo $objAlumno->nombre;// imprimir una propiedad
+
+$objAlumno2 = new Persona();
+
+$objAlumno2->asignarNombre("Pablo");
+
+echo $objAlumno2->nombre;
+
+echo $objAlumno->mostrarNombre();
